571c2b9ca9426169f1d97e7c85e0ea6e09fb2ea2
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-01-29 16:57:53 +0100

    Update set-cookie.js

--- api/set-cookie.js
+++ api/set-cookie.js
@@ -1,7 +1,7 @@
 const { serialize } = require('cookie')
 
 module.exports = (req, res) => {
-  const cookie = serialize('ck', 'value', {httpOnly: true, expires: new Date('2023-12-01')})
+  const cookie = serialize('ck', 'value', {httpOnly: true, maxAge: 1000 * 60}) // 1 minute
   res.setHeader('Set-Cookie', [cookie]);
   res.end('It worked, check your cookies in devtools');
 }
