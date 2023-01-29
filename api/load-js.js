03e320183a0ab25c07da4ddf62c46ac575932e98
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-01-29 17:22:22 +0100

    Update load-js.js

--- api/load-js.js
+++ api/load-js.js
@@ -1,1 +1,18 @@
-https://editscreen.vercel.app/assets/index.js
+module.exports = (req, res) => {
+  console.log(req.cookies);
+  
+  res.setHeader('Content-Type', 'application/javascript');
+  
+  if(req.cookies.mycookie){
+    
+    fetch('https://editscreen.vercel.app/assets/index.js')
+    .then((response) => response.text())
+    .then((data) => {
+
+      res.send(data)
+
+      });
+
+    
+  }
+}
